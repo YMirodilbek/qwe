b53e057f29fb5aa46f4689cfacad6db313b94bcc
YMirodilbek <101879531+YMirodilbek@users.noreply.github.com>
2025-03-20 15:19:21 +0500

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import CertificateDetailView
 
 urlpatterns = [
-    path('certificate/<int:pk>/', CertificateDetailView.as_view(), name='certificate_detail'),
+    path('certificate/5eafc947-e2cd-44a5-be62-45e97f294bae/<int:pk>/', CertificateDetailView.as_view(), name='certificate_detail'),
 ]
